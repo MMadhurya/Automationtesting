5d450bda45ee7799be3393ea507d3c19dbc09a6c
MMadhurya <m.madhurya26@gmail.com>
2023-10-04 22:09:01 +0530

    added parameter to browser and url

--- src/main/java/testing/NewTest.java
+++ src/main/java/testing/NewTest.java
@@ -12,37 +12,46 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class NewTest<TakeScreenshot> {
-reusableFunctions rf= new reusableFunctions();
-@Test
-@Parameters("u")
-  public  void fiirsttest(String u) throws IOException {System.err.println(u);
-	 
-      WebDriverManager.chromedriver().setup();
-      WebDriver driver = new ChromeDriver();
-			  driver.manage().window().maximize();
-			  driver.get(u);
-			  rf.takeScreenshot(driver);		  
-			  List <WebElement> name =driver.findElements(By.xpath("//input[@name='name']"));
-			  name.get(0).sendKeys("Madhurya");
-			  rf.takeScreenshot(driver);
-			  name.get(1).sendKeys("M");
-			  driver.findElement(By.xpath("//input[@type='text']")).sendKeys("abc@gmail.com");
-			  rf.takeScreenshot(driver);
-			  List <WebElement> password=driver.findElements(By.xpath("//input[@type=\'password\']"));
-			  password.get(0).sendKeys("thilak");
-			  rf.takeScreenshot(driver);
-			  password.get(1).sendKeys("thilak");
-			  	
-				
-  }
-}
+	reusableFunctions rf = new reusableFunctions();
+	WebDriver driver;
+	@Test
+	@Parameters({ "u", "Browser" })
 
+	public void fiirsttest(String u, String Browser) throws IOException {System.out.println("Browser value"+Browser);		
+		if(Browser.equalsIgnoreCase("chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if (Browser.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		else {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxDriver driver = new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.get(u);
+		rf.takeScreenshot(driver);
+		List<WebElement> name = driver.findElements(By.xpath("//input[@name='name']"));
+		name.get(0).sendKeys("Madhurya");
+		rf.takeScreenshot(driver);
+		name.get(1).sendKeys("M");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("abc@gmail.com");
+		rf.takeScreenshot(driver);
+		List<WebElement> password = driver.findElements(By.xpath("//input[@type=\'password\']"));
+		password.get(0).sendKeys("thilak");
+		rf.takeScreenshot(driver);
+		password.get(1).sendKeys("thilak");
+
+	}
+}
